b50037fa2569081a095add07de591b2090b9504e
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-10-09 01:13:15 +0600

    started with the dicee app

---
+++ dicee_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    // TODO: implement build
+    return Container();
+  }
+
+}
