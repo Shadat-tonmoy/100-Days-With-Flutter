5df9fe7cfafbd97024c807972b8b6a15644bcd5b
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-10-13 23:43:22 +0600

    started with the quizzler app from scratch to build the initial ui

---
+++ quizzler_app/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+
+void main()
+{
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.grey[900],
+      body: SafeArea(
+          child: QuizzlerPage()),
+    ),
+  ));
+}
+
+
+class QuizzlerPage extends StatefulWidget
+{
+  @override
+  _QuizzlerPageState createState() => _QuizzlerPageState();
+}
+
+class _QuizzlerPageState extends State<QuizzlerPage>
+{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 8,
+          child: Center(
+            child: Text(
+              "Question Goes Here there....",
+              style: TextStyle(
+                color: Colors.white
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: ()
+              {
+                print("Pressed");
+
+              },
+              color: Colors.green,
+              child: Text(
+                "True",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: ()
+              {
+                print("Pressed");
+              },
+              color: Colors.red,
+              child: Text(
+                "False",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0
+                ),
+              ),
+
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
