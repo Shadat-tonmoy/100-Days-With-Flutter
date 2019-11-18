4f1427dd7063b2f3b3b13489f7f62cad1a5f4e30
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-11-18 09:13:33 +0600

    show loading screen on login or signup progress

--- flash_chat/lib/screens/login_screen.dart
+++ flash_chat/lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/constants/constant_values.dart';
 import 'package:flash_chat/customWidgets/buttonWidgets.dart';
 import 'package:flash_chat/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -14,19 +15,44 @@ class _LoginScreenState extends State<LoginScreen>
 
   String _email, _password;
   FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  bool _showLoadingIndicator = false, _authResultVisibility = false;
+
+  void updateLoadingIndicatorState(bool state)
+  {
+    setState(() {
+      _showLoadingIndicator = state;
+    });
+  }
+
+  void updateAuthResultState(bool state)
+  {
+    setState(() {
+      _authResultVisibility = state;
+    });
+  }
 
   Future<void> loginUser() async
   {
+    updateLoadingIndicatorState(true);
+    updateAuthResultState(false);
     FirebaseUser firebaseUser = await _firebaseAuth.currentUser();
     if(firebaseUser == null)
     {
-        AuthResult authResult = await _firebaseAuth.signInWithEmailAndPassword(email: _email, password: _password);
-        firebaseUser = authResult.user;
-        if(firebaseUser!=null) {
-          Navigator.pushNamed(context, ScreenRoutes.CHAT_SCREEN);
+        try
+        {
+          AuthResult authResult = await _firebaseAuth.signInWithEmailAndPassword(email: _email, password: _password);
+          firebaseUser = authResult.user;
+          if(firebaseUser!=null)
+          {
+            updateLoadingIndicatorState(false);
+            Navigator.pushNamed(context, ScreenRoutes.CHAT_SCREEN);
+          }
         }
-        else {
-          print("Login error ${authResult.toString()}");
+        catch(e)
+        {
+          print("Login error ${e.toString()}");
+          updateLoadingIndicatorState(false);
+          updateAuthResultState(true);
         }
     }
     else {
@@ -48,61 +74,80 @@ class _LoginScreenState extends State<LoginScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          child: SingleChildScrollView( 
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Hero(
-                  tag: AnimationTags.ANIMATED_LOGO,
-                  child: Container(
-                    height: 200.0,
-                    child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Hero(
+                    tag: AnimationTags.ANIMATED_LOGO,
+                    child: Container(
+                      height: 200.0,
+                      child: Image.asset('images/logo.png'),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 48.0,
-                ),
-                TextField(
-                  onChanged: (value) {
-                    _email = value;
-                  },
-                  style: TextStyle(
-                    color: Colors.grey[800]
+                  SizedBox(
+                    height: 32.0,
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: kInputFieldDecoration.copyWith(hintText: "Enter your email."),
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                TextField(
-                  onChanged: (value) {
-                    _password = value;
-                  },
-                  style: TextStyle(
-                    color: Colors.grey[800]
+                  Visibility(
+                    maintainAnimation: true,
+                    maintainState: true,
+                    visible: _authResultVisibility,
+                    child: Text(
+                      "Login Failed! Please Check Your Email and Password and Try Again!",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red
+                      ),
+                    ),
                   ),
-                  obscureText: true,
-                  decoration: kInputFieldDecoration.copyWith(hintText: "Enter your password."),
-                ),
-                SizedBox(
-                  height: 24.0,
-                ),
-                AppMaterialButton(
-                  color: Colors.lightBlueAccent,
-                  text: "Log In",
-                  onPressedCallback: (){
-                    loginUser();
-                  },
-                ),
-              ],
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  TextField(
+                    onChanged: (value) {
+                      _email = value;
+                    },
+                    style: TextStyle(
+                      color: Colors.grey[800]
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: kInputFieldDecoration.copyWith(hintText: "Enter your email."),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  TextField(
+                    onChanged: (value) {
+                      _password = value;
+                    },
+                    style: TextStyle(
+                      color: Colors.grey[800]
+                    ),
+                    obscureText: true,
+                    decoration: kInputFieldDecoration.copyWith(hintText: "Enter your password."),
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  AppMaterialButton(
+                    color: Colors.lightBlueAccent,
+                    text: "Log In",
+                    onPressedCallback: (){
+                      loginUser();
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
+        inAsyncCall: _showLoadingIndicator,
       ),
     );
   }
--- flash_chat/lib/screens/registration_screen.dart
+++ flash_chat/lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/constants/constant_values.dart';
 import 'package:flash_chat/customWidgets/buttonWidgets.dart';
 import 'package:flash_chat/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   @override
@@ -14,14 +15,22 @@ class _RegistrationScreenState extends State<RegistrationScreen>
 
   String email, password;
   FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  bool _showRegistrationLoading = false;
 
-  Future<void> signupWithEmailAndPassword() async {
+  void updateLoadingState(bool state)
+  {
+    setState(() {_showRegistrationLoading = state;});
+  }
+
+  Future<void> signUpWithEmailAndPassword() async {
     print("Grabbed Valeu $email password $password instance $_firebaseAuth");
     try
     {
       if(_firebaseAuth==null)
         _firebaseAuth = FirebaseAuth.instance;
+      updateLoadingState(true);
       final newUser = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+      updateLoadingState(false);
       if(newUser != null)
         Navigator.pushNamed(context, ScreenRoutes.CHAT_SCREEN);
     }
@@ -38,63 +47,66 @@ class _RegistrationScreenState extends State<RegistrationScreen>
   @override
   Widget build(BuildContext context) {
     print("FirebaseAuth $_firebaseAuth");
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Hero(
-                  tag: AnimationTags.ANIMATED_LOGO,
-                  child: Container(
-                    height: 200.0,
-                    child: Image.asset('images/logo.png'),
+    return ModalProgressHUD(
+      inAsyncCall: _showRegistrationLoading,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Hero(
+                    tag: AnimationTags.ANIMATED_LOGO,
+                    child: Container(
+                      height: 200.0,
+                      child: Image.asset('images/logo.png'),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 48.0,
-                ),
-                TextField(
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  style: TextStyle(
-                      color: Colors.grey[800]
+                  SizedBox(
+                    height: 48.0,
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: kInputFieldDecoration.copyWith(hintText: "Enter your email"),
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                TextField(
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  style: TextStyle(
-                    color: Colors.grey[800],
+                  TextField(
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    style: TextStyle(
+                        color: Colors.grey[800]
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: kInputFieldDecoration.copyWith(hintText: "Enter your email"),
                   ),
-                  obscureText: true,
-                  decoration: kInputFieldDecoration.copyWith(hintText: "Enter your password"),
-                ),
-                SizedBox(
-                  height: 24.0,
-                ),
-                AppMaterialButton(
-                  color: Colors.blueAccent,
-                  text: "Register",
-                  onPressedCallback: () async
-                  {
-                    await signupWithEmailAndPassword();
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  TextField(
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                    ),
+                    obscureText: true,
+                    decoration: kInputFieldDecoration.copyWith(hintText: "Enter your password"),
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  AppMaterialButton(
+                    color: Colors.blueAccent,
+                    text: "Register",
+                    onPressedCallback: () async
+                    {
+                      await signUpWithEmailAndPassword();
 
 
-                  },
-                ),
-              ],
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
