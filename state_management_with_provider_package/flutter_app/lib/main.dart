3e9316f16db6999c3b0b07e0c45dbc53ea6df958
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-11-29 14:30:52 +0600

    implemented the basic provider package to access static or stateless data along the widget tree

--- state_management_with_provider_package/flutter_app/lib/main.dart
+++ state_management_with_provider_package/flutter_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,16 +7,34 @@ class MyApp extends StatelessWidget
 {
 
   String data = "My Top Secret Data";
+//  String data = "123456";
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(data),
-        ),
-        body: Container(
-          child: Level1(data: data),
+    return Provider<String>(
+      builder: (context) => data,
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(data),
+          ),
+          body: Column(
+            children: <Widget>[
+              Level1(),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 16.0,vertical: 32.0),
+                child: TextField(
+                  onChanged: (newText){
+
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Enter Text Here"
+                  ),
+
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -25,43 +44,33 @@ class MyApp extends StatelessWidget
 class Level1 extends StatelessWidget
 {
 
-  final String data;
-
-  Level1({this.data});
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Level2(data: data),
+      margin: EdgeInsets.all(16.0),
+      child: Level2(),
     );
   }
 }
 
 class Level2 extends StatelessWidget
 {
-  final String data;
-
-  Level2({this.data});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Level3(data: data,),
+      child: Level3(),
     );
   }
 }
 
 class Level3 extends StatelessWidget
 {
-  final String data;
-
-  Level3({this.data});
-
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Text(
-        data
+        Provider.of<String>(context)
       ),
     );
   }
