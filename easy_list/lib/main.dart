61ee9aed9cd15bc20bfe89112859b63e26cb5ca0
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-12-05 09:26:11 +0600

    added homescreen card with image on it

--- easy_list/lib/main.dart
+++ easy_list/lib/main.dart
@@ -1,35 +1,37 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
-            "Easy List"
-          ),
+          title: Text("Easy List"),
         ),
         body: HomeScreen(),
       ),
     );
   }
-
 }
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          "Easy List"
+    return Card(
+        child: Column(
+      children: <Widget>[
+        Image.asset("assets/images/food.jpg"),
+        SizedBox(
+          height: 20,
         ),
-      ),
-    );
+        Text(
+          "Food Paradise"
+        )
+      ],
+    ));
   }
 }
