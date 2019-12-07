0f9aa78bfc9bc281ae2ecae0ae37681e77620fea
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-12-07 13:52:04 +0600

    perfect centerring for no element found layout is implemented

--- easy_list/lib/main.dart
+++ easy_list/lib/main.dart
@@ -48,28 +48,31 @@ class HomeScreen extends StatelessWidget
   Widget getUIWidget(){
    if(foodItems.length == 0){
      return Center(
-       child: Column(
-         mainAxisAlignment: MainAxisAlignment.center,
-         children: <Widget>[
-           Image(
-             image: AssetImage("assets/images/no_item.png"),
-             fit: BoxFit.fill,
-             width: 250,
-             height: 200,
-           ),
-           Padding(
-             padding: const EdgeInsets.all(8.0),
-             child: Text(
-                "No Element Found!\nPlease tap the '+' button to add element",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey[600]
-                ),
-
+       child: Container(
+         margin: EdgeInsets.only(bottom: 50),
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+             Image(
+               image: AssetImage("assets/images/no_item.png"),
+               fit: BoxFit.fill,
+               width: 250,
+               height: 200,
              ),
-           )
-         ],
+             Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: Text(
+                  "No Element Found!\nPlease tap the '+' button to add element",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[600]
+                  ),
+
+               ),
+             )
+           ],
+         ),
        ),
      );
    }
