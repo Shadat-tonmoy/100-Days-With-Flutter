115c4cad6bc87d06223e64d40290b4d90a5fbf67
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-12-11 20:17:08 +0600

    implemented the basic ui for product details screen

--- easy_list/lib/screens/productDetailScreen.dart
+++ easy_list/lib/screens/productDetailScreen.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
 
-class ProductDetail extends StatelessWidget
-{
+class ProductDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Product Details"),
       ),
-      body: Container(
-        child: Center(
-          child: Text(
-            "Product Detail Goes Here"
-          ),
-        ),
-      ),
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 5.0,
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset("assets/images/food.jpg"),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Food Paradise"
+                    ),
+                  )
+                ],
+              ),
+            )
+          ]),
     );
   }
 }
