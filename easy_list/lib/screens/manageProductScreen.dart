d455c8b72dc8af92113e305e0ccaa58460f32eaa
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-12-14 19:39:18 +0600

    added tabbar on manage product page

--- easy_list/lib/screens/manageProductScreen.dart
+++ easy_list/lib/screens/manageProductScreen.dart
@@ -4,14 +4,27 @@ class ManageProduct extends StatelessWidget {
   @override
   Widget build(BuildContext context)
   {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Manage Product"
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Manage Product"
+          ),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text:"Add Product",
+              ),
+              Tab(
+                text: "All Products",
+              )
+            ],
+          ),
         ),
-      ),
-      body: Container(
+        body: Container(
 
+        ),
       ),
     );
   }
