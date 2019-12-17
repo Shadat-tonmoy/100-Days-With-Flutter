1c671166660bd1143e6e8c5947109574909e434a
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-12-17 09:18:15 +0600

    added named route for other pages like home screen, manage product screen etc, skipped the passing parameter with named route portion as this doesnot seems intuitive though

--- easy_list/lib/tasks/appRoutingTask.dart
+++ easy_list/lib/tasks/appRoutingTask.dart
@@ -1,3 +1,4 @@
+import 'package:easy_list/constants/constants.dart';
 import 'package:easy_list/screens/homeScreen.dart';
 import 'package:easy_list/screens/manageProductScreen.dart';
 import 'package:flutter/material.dart';
@@ -8,16 +9,18 @@ class AppRouter
   void toManageProductScreen(BuildContext context)
   {
     Navigator.pop(context);
-    Navigator.push(context, MaterialPageRoute(
+    /*Navigator.push(context, MaterialPageRoute(
       builder: (context) => ManageProduct()
-    ));
+    ));*/
+    Navigator.pushNamed(context, RoutingKeys.MANAGE_PRODUCT_SCREEN);
   }
 
   void toHomeScreen(BuildContext context)
   {
-    Navigator.pushReplacement(context, MaterialPageRoute(
+    /*Navigator.pushReplacement(context, MaterialPageRoute(
       builder: (context) => HomeScreen()
-    ));
+    ));*/
+    Navigator.pushNamed(context, RoutingKeys.HOME_SCREEN);
   }
 
   Function getManageProductRouter()
