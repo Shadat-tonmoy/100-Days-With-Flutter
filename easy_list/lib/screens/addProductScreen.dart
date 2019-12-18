d18f07beb0a6458e706ef6b1b686ef07057bf4e6
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-12-18 09:21:57 +0600

    added add product screen edittext fields

--- easy_list/lib/screens/addProductScreen.dart
+++ easy_list/lib/screens/addProductScreen.dart
@@ -1,11 +1,54 @@
 import 'package:flutter/material.dart';
 
-class AddProductScreen extends StatelessWidget {
+class AddProductScreen extends StatefulWidget
+{
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Add Product Here"
+  _AddProductScreenState createState() => _AddProductScreenState();
+}
+
+class _AddProductScreenState extends State<AddProductScreen>
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Product Title",
+                prefixIcon: Icon(Icons.edit),
+                contentPadding: EdgeInsets.all(16.0),
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Product Description",
+                prefixIcon: Icon(Icons.edit),
+                contentPadding: EdgeInsets.all(16.0),
+              ),
+              maxLines: 5,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Product Price",
+                prefixIcon: Icon(Icons.edit),
+                contentPadding: EdgeInsets.all(16.0),
+              ),
+              keyboardType: TextInputType.numberWithOptions(),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Delivery Address",
+                prefixIcon: Icon(Icons.place),
+                contentPadding: EdgeInsets.all(16.0),
+              ),
+              keyboardType: TextInputType.numberWithOptions(),
+            )
+          ],
+        ),
       ),
     );
   }
