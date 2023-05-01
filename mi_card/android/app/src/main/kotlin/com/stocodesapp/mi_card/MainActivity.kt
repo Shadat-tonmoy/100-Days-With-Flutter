31161ace2edda5f7969ff91f68977c449a860b88
shadattonmoy <shadat.tonmoy@gmail.com>
2023-05-01 13:15:57 +0600

    updated target sdk version

--- mi_card/android/app/src/main/kotlin/com/stocodesapp/mi_card/MainActivity.kt
+++ mi_card/android/app/src/main/kotlin/com/stocodesapp/mi_card/MainActivity.kt
@@ -2,12 +2,11 @@ package com.stocodesapp.mi_card
 
 import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+//  override fun onCreate(savedInstanceState: Bundle?) {
+//    super.onCreate(savedInstanceState)
+////    GeneratedPluginRegistrant.registerWith(this)
+//  }
 }
